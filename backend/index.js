c5aee0a0088fbb931c47fcdd77cb75f648ccdd06
NammikaParthiv <nammikaparthiv@gmail.com>
2026-07-29 02:15:55 +0530

    Added clientURL of the vercel

--- backend/index.js
+++ backend/index.js
@@ -21,8 +21,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
+    origin: process.env.CLIENT_URL,
     credentials: true,
-    origin:"http://localhost:5173"
 }));
 
 connect2DB();
